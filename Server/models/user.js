8ec4f7257601897eb9524a53db94e9e6df3b52f8
nightfury-toothless <lukas19940702thomas@gmail.com>
2021-09-19 10:20:35 +0530

    refactor: minor optimizaton

--- Server/models/user.js
+++ Server/models/user.js
@@ -10,51 +10,22 @@ const userSchema = new Schema(
       required: true,
       trim: true,
     },
-    // encry_accesstoken: {
-    //   type: String,
-    //   required: true,
-    // },
     accesstoken: {
       type: String,
       required: true,
     },
-    // salt: String,
-
-    templates: {
-      type: Array,
-      default: [],
-    },
+    templates: [
+      {
+        templateName: {
+          type: String,
+          trim: true,
+          maxlength: 100,
+        },
+        templateJson: {},
+      },
+    ],
   },
   { timestamps: true }
 );
-
-// userSchema
-//   .virtual("accesstoken")
-//   .set(function (accesstoken) {
-//     this._accesstoken = accesstoken;
-//     this.salt = uuidv1();
-//     this.encry_accesstoken = this.securePassword(accesstoken);
-//   })
-//   .get(function () {
-//     return this._accesstoken;
-//   });
-
-// userSchema.methods = {
-//   authenticate: function (accesstoken) {
-//     return this.securePassword(accesstoken) === this.encry_accesstoken;
-//   },
-//   securePassword: function (accesstoken) {
-//     if (!accesstoken) return "";
-
-//     try {
-//       return crypto
-//         .createHmac("sha256", this.salt)
-//         .update(accesstoken)
-//         .digest("hex");
-//     } catch (error) {
-//       return "";
-//     }
-//   },
-// };
 
 module.exports = mongoose.model("User", userSchema);
